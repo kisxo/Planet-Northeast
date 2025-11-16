ff97eb901142ddc48c34199fbfc6417c3e15c194
Sosanka <santumoni03@gmail.com>
2025-11-16 19:25:37 +0530

    new init

--- src/components/Tours.tsx
+++ src/components/Tours.tsx
@@ -1,5 +1,6 @@
 import Anini from "@/assets/anini.jpg";
 import tawangRoad from "@/assets/tawang.jpg";
+import tawangGroup from "@/assets/tawang.jpg";
 import Machuka from "@/assets/Machuka.jpg";
 import Meghalaya from "@/assets/meghalaya.jpg";
 import { Button } from "@/components/ui/button";
@@ -11,8 +12,16 @@ const tours = [
     title: "Tawang Tour",
     image: tawangRoad, // Replace with your image import
     duration: "5 Days | 4 Nights",
-    groupSize: "Private/Group Tour",
+    groupSize: "Private Tour",
     price: "From ₹9,999 /- Per Person",
+    link: "/tours/tawang",
+  },
+  {
+    title: "Hidden Himalayas: The Offbeat Tawang Trails- Group Tour",
+    image: tawangGroup, // Replace with your image import
+    duration: "6 Days | 5 Nights",
+    groupSize: "Group Tour",
+    price: "From ₹13,999 /- Per Person",
     link: "/tours/tawang",
   },
   {
@@ -42,8 +51,7 @@ const tours = [
 ];
 
 const Tours = () => {
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <section id="tours" className="py-16 bg-muted/30">
       <div className="container mx-auto px-6">
@@ -62,7 +70,10 @@ const Tours = () => {
               className="bg-card rounded-2xl overflow-hidden shadow-soft hover:shadow-hover transition-all duration-300 group cursor-pointer"
             >
               <div className="relative overflow-hidden aspect-[4/3]">
-                <img onClick={()=>{navigate(tour.link);}}
+                <img
+                  onClick={() => {
+                    navigate(tour.link);
+                  }}
                   src={tour.image}
                   alt={tour.title}
                   className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
